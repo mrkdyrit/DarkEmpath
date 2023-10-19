57f2320ee7cf5d490edb244e94d22b3ac83af357
Mark Angelo Dayrit <markangelodayrit00@gmail.com>
2023-10-19 11:19:20 +0800

    Added inbox data

---
+++ lib/data/chat/inbox_data.dart
@@ -0,0 +1,29 @@
+class InboxData {
+  List<Map<String, dynamic>> inbox = [
+    {
+      ''
+      'name': 'REDFORD MORATALLA',
+      'image': '',
+      'last_message': 'Hello World',
+      'time': '11:59 PM',
+    },
+    {
+      'name': 'REDFORD MORATALLA',
+      'image': '',
+      'last_message': 'Hello World',
+      'time': '11:59 PM',
+    },
+    {
+      'name': 'REDFORD MORATALLA',
+      'image': '',
+      'last_message': 'Hello World',
+      'time': '11:59 PM',
+    },
+    {
+      'name': 'REDFORD MORATALLA',
+      'image': '',
+      'last_message': 'Hello World',
+      'time': '11:59 PM',
+    }
+  ];
+}
